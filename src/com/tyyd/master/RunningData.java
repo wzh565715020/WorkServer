8649a0d0de5eb2901a2653c775b844d6c8b96c51
wzh565715020 <38375952+wzh565715020@users.noreply.github.com>
2018-11-30 17:27:56 +0800

    Initial commit

---
+++ src/com/tyyd/master/RunningData.java
@@ -0,0 +1,30 @@
+package com.tyyd.master;
+
+import java.io.Serializable;
+public class RunningData implements Serializable {
+
+    private static final long serialVersionUID = 4260577459043203630L;
+
+
+    //服务器id
+    private long cid;
+    //服务器名称
+    private String name;
+
+
+    public long getCid() {
+        return cid;
+    }
+
+    public void setCid(long cid) {
+        this.cid = cid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
